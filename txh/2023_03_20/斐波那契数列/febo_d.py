02993d681a0b10b10f82cd40240ae1e954695179
szoloa <szoloa@hotmail.com>
2023-03-19 22:33:06 +0800

    modified:   "txh/2023_03_20/\346\226\220\346\263\242\351\202\243\345\245\221\346\225\260\345\210\227/febo_d.py" 	modified:   "txh/2023_03_20/\346\226\220\346\263\242\351\202\243\345\245\221\346\225\260\345\210\227/febo_da.py"

--- txh/2023_03_20/斐波那契数列/febo_d.py
+++ txh/2023_03_20/斐波那契数列/febo_d.py
@@ -2,4 +2,6 @@ def feb(n):
     if n == 1 or n == 2:
         return 1
     return feb(n-1) + feb(n-2)
-print(feb(int(input())))
+n = int(input())
+for i in range(1,n+1):
+    print(feb(n))
--- txh/2023_03_20/斐波那契数列/febo_da.py
+++ txh/2023_03_20/斐波那契数列/febo_da.py
@@ -2,5 +2,5 @@ febn = 1
 febn_ = 1
 n = int(input())
 for i in range(n):
+    print(febn)
     febn, febn_ = febn_+febn, febn
-print(febn)
